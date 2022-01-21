bff2c108604184e90497cf1e6228e822319e37e6
mallik-desaraju <desaraju670@gmail.com>
2022-01-21 13:07:11 +0530

    Create MyClass.java

---
+++ MyClass.java
@@ -0,0 +1,31 @@
+public class MyClass {
+    public static void main(String args[]) {
+      int x=10;
+      int y=25;
+            System.out.println( " Before x y = " + x+ " " + y );
+
+      //int c = x;
+      //x = y;
+      //y = c;
+
+      x = x + y;
+      y = x - y;
+      x = x - y;
+      
+      System.out.println("After Swap x y = " + x + " " +  y );
+      
+            System.out.println("x * y = " + x*+ y );
+            
+                  System.out.println("x y = " + x / y );
+                  String hubby= " Sateesh";
+                  System.out.println("Hi Pandu from " + (hubby));
+
+    for (int i=1; i<= 100; i++){
+                         // System.out.print(i + " ");
+                  System.out.println(i + " Hi Pandu from " + (hubby));
+
+    }
+
+      
+    }
+}
